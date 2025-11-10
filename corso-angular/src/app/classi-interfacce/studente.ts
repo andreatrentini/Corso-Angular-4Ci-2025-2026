de1bf05eb7e8d1fb88872d8fa907395501467e98
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2025-11-10 11:20:39 +0000

    FEAT (Studente): aggiunto il controllo della classe

--- corso-angular/src/app/classi-interfacce/studente.ts
+++ corso-angular/src/app/classi-interfacce/studente.ts
@@ -9,7 +9,8 @@ export class Studente extends Persona {
     constructor(id: number, nome: string, cognome: string, sesso: string, classe: string) {
         // super richiama il costruttore della classe padre
         super(id, nome, cognome, sesso);
-        this._classe = classe;
+        this._classe = '';
+        this.cambiaClasse(classe);
     }
 
     public get classe(): string {
@@ -20,6 +21,23 @@ export class Studente extends Persona {
         // Controllo se il nome della classe è corretto
         // 4Bi
         // Suggerimento: usate le regex
+        const regex = /^[1-5][ABCDEQ][iame]?$/;
+        /*
+        ^ (Ancora): Indica l'inizio della stringa.
+        \d (Digit): corrisponde a un singolo carattere che sia '1', '2', '3', '4', o '5'..
+        [ABCDEQ] (Classe di caratteri): Corrisponde esattamente a una delle lettere maiuscole specificate (A, B, C, D, E, oppure Q).
+        [iame] (Classe di caratteri): Corrisponde esattamente a una delle lettere minuscole specificate (i, a, m, oppure e).
+        ? (Quantificatore): Rende opzionale il gruppo che lo precede. 
+                            In questo caso, rende opzionale [iame]. 
+                            Significa "zero oppure uno" di quel gruppo.
+        $ (Ancora): Indica la fine della stringa.
+        */
+        if (regex.test(value)) {
+            this._classe = value;
+        }
+        else {            
+            throw new Error('Nome della classe errato.');
+        }
     }
 
 }
