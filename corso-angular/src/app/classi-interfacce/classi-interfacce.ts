b5a87547ea3d9195eca3d0957aafcb1a76373a7c
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2025-11-17 11:15:53 +0000

    FEAT: Gestione classe completo

--- corso-angular/src/app/classi-interfacce/classi-interfacce.ts
+++ corso-angular/src/app/classi-interfacce/classi-interfacce.ts
@@ -1,5 +1,7 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { ClasseScolastica } from './classe-scolastica';
+import { Studente } from './studente';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-classi-interfacce',
@@ -10,13 +12,23 @@ import { ClasseScolastica } from './classe-scolastica';
 export class ClassiInterfacce {
   // se non si vuole assegnare un valore ad un attributo è sufficiente aggiungere un punto esclamativo al nome
   classe: WritableSignal<ClasseScolastica | undefined> = signal(undefined);
+  id: number = 1;
+  visualizzaErrore: WritableSignal<string> = signal('');
 
   creaClasse(nomeClasse: string, annoScolastico: string) {   
     try {
       this.classe.set(new ClasseScolastica(nomeClasse, annoScolastico));
-    } catch (error) {
-      console.log(error)
+    } catch (error: any) {
+      // Errore durante la creazione della classe
+      this.visualizzaErrore.set(error);
+      timer(4000).subscribe(() => {
+        this.visualizzaErrore.set('');
+      })
     }
   }
-  
+
+  aggiungiStudente(nome: string, cognome: string, sesso: string): void {
+      this.classe()!.aggiungiStudente(new Studente(this.id, nome, cognome, sesso, this.classe()!.nomeClasse))
+      this.id++;
+  }  
 }
