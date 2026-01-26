1be2c8fac7c0cfe654bb27ded653d0379a4bd364
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2026-01-26 09:53:53 +0000

    FEAT: album-grid e album-card

---
+++ spotify/src/app/artist/albums-grid/albums-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-grid',
+  imports: [],
+  templateUrl: './albums-grid.html',
+  styleUrl: './albums-grid.css',
+})
+export class AlbumsGrid {
+
+}
